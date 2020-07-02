03923fe7c621913c6895ffb4f9b56118160b8d48
Daniel Gomez <danipipe1703@gmail.com>
2020-07-01 23:38:28 -0500

    front TTT Y backchat

---
+++ src/main/java/co/edu/arsw/ticktackGame/TicktackGameApplication.java
@@ -0,0 +1,13 @@
+package co.edu.arsw.ticktackGame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicktackGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicktackGameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/arsw/ticktackGame/TicktackGameApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.arsw.ticktackGame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicktackGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
